122dc12228abcdf8b7400ea5d2b2f5fc59ff9082
Douglas Edipo <72673667+DouglasEd@users.noreply.github.com>
2024-11-14 18:23:51 -0300

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -7,7 +7,7 @@ xset -dpms
 
 unclutter -idle 0.5 -root &
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pythonisa/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pythonisa/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/{user}/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/{user}/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk localhost/Pythonisa
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk {Link a ser aberto}
